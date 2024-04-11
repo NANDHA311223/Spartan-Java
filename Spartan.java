1482e214d0dfb3f6c0f86117326d11726cbb295e
NANDHA KUMAR S <162905089+NANDHA311223@users.noreply.github.com>
2024-04-11 18:24:11 +0530

    Add files via upload

---
+++ Spartan.java
@@ -0,0 +1,21 @@
+//JAVA FIRST PROGRAM
+import java.io.*;
+public class Spartan{ 
+	public static void main(String[]args){ 
+		 System.out.println("SPARTAN");
+	}
+}
+/*   ***NOTES***
+1.>public    = publicly available/accesable
+2.>static    = call the mthd without creating an obj
+3.>void      = having no return type
+4.>main      = execution always strts @main mthd
+5.>string    = seqnce of chars
+6.>args      = i/p to the progrm to what to do
+7.>sequence of chars must be in double cotation
+8.>syntax    = rules of the prgrm
+9.>()        = paranthases
+10.>{}       = curley/flwr-braces/bracket
+11.>inside the paranthases is called args/parametrers
+12.>out      = instance of print stream class
+*/
