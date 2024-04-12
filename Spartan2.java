aed99890f07f1bf2cd5efe0f6dd2088a9756c0d0
NANDHA KUMAR S <162905089+NANDHA311223@users.noreply.github.com>
2024-04-12 22:49:22 +0530

    Add files via upload

---
+++ Spartan2.java
@@ -0,0 +1,21 @@
+//JAVA VARIABLES AND DATATYPES
+public class Spartan2 {
+ public static void main(String[]args){
+	 int ticketPrice  = 300;
+	 boolean paid = true;
+	 char c = 't';
+	 System.out.println(" ticketPrice " +ticketPrice+ " Rs ");
+ }
+}
+/*   ***NOTES***
+1.>int = data type
+2.>ticketPrice = variable
+3.>300 = literals
+4.>ticetPrice = camelcase
+5.>int ticketPrice; = //variable declaration - Data type+variable
+6.>int ticketPrice = 300  (int ticketPrice + added a value called  [initializatioin])
+7.>commands = program clarity/reference purpose
+8.>ASCII = american standard code for infoermation interchange
+9.>Class name always starts at caps (this is a Convention)
+10.>char litrl = single cotation , string litrl = double cotation
+*/
