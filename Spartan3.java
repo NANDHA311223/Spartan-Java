dee8fa18f31d1634a83250914ad8b1b21c3d13fb
NANDHA KUMAR S <162905089+NANDHA311223@users.noreply.github.com>
2024-04-13 23:32:42 +0530

    Add files via upload

---
+++ Spartan3.java
@@ -0,0 +1,39 @@
+//JAVA COMMENTS & LITERALS
+public class Spartan3 {
+ public static void main(String[]args) {
+	 byte b;
+	 int n = 10;
+	 b = (byte)n;
+	 System.out.println(b);
+ }
+}
+/* ***NOTES***
+     // = Single line command
+     /* = multi line cmnd
+     prgrm giving values = ltrls
+     
+1.>hello/nworld = /n is a escape sequesnce
+2.>10 = number ltrl
+3.>o10 = octal number
+4.>0xA = hexa decimal
+5.>long 7l /7L = l/L (considered long number)
+6.>0b = binary number (ob + 010102)
+7.>float pi = (to store floating pointr number)
+    float pi = 3.14523;    (standrd notation)
+    float pi = 314523e-05; (scienbntific notation)
+    
+    
+            >>>TYPE CASTING<<<
+========implisit/automatic - type convrsn=========
+byte b = 5;  //1bytes
+int n;       //4bytes
+n=b;        (byte number automaticaly changed in to integer)
+                 implisit/automatic - type conversion
+  small value stored in big value but not big value stored in small
+  
+
+===============explisit type cnvrsn===============
+byte b;
+int n = 10;
+b = (byte)n; 
+*/
