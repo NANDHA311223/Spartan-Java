1a9108c64138e3de6e0aabc6f4f1770ca72071f2
NANDHA KUMAR S <162905089+NANDHA311223@users.noreply.github.com>
2024-04-16 20:18:58 +0530

    Add files via upload

---
+++ Spartan6.java
@@ -0,0 +1,22 @@
+public class Spartan6 {
+	public static void main(String[]args) {
+		int a=18,b=15;
+		System.out.println(a!=b);
+	}
+}
+/*
+RELATIONAL OPERATORS:
+
+<  = Lesser then
+<= = Lesser then or equal to
+>  = Greater then 
+>= = Greater then or equal to
+
+EQUALITY CHECKING OPERATORS :
+
+==  = Equal
+!=  = Not Equal
+
+~in if class we are mostly used relational operators
+~Realational operators will give only true or false
+*/ 
