93c3c4a91f0497735b5ec5f09c36b20d7ad5c784
NANDHA KUMAR S <162905089+NANDHA311223@users.noreply.github.com>
2024-04-20 14:35:49 +0530

    Add files via upload

---
+++ Spartan9.java
@@ -0,0 +1,17 @@
+//INCREMENT DECREMENT 
+
+//Operator Precedence
+public class Spartan9 {
+ public static void main(String[]args) {
+	 int a=15;
+	 a= a++ + ++a;
+	 System.out.println(a);
+ }
+}
+/*
+a++; = Post increment 
+a--; = pre increment
+
+++a; = Post decrement
+--a; = Post decrement
+*/
