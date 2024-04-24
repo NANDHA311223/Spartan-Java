8b08992ed72219b4e433c83edd794ba48d55c0fc
NANDHA KUMAR S <162905089+NANDHA311223@users.noreply.github.com>
2024-04-24 21:32:58 +0530

    Add files via upload

---
+++ Spartan13.java
@@ -0,0 +1,49 @@
+//Loops
+/*
+ 1.Used to repeat set of instructions again and again
+ 2.3 Loops = for ,While,Dowhile
+ 3.counter(i) = Iterator
+ */
+public class Spartan13 {
+    public static void main(String[]args){
+/*********************For Loop**********************/
+    //	int counter; = this will be declared outside the loop 
+    	for( int counter =1;counter<=15;counter++) {
+           // initialization  Condition   inc/Dec or Otrs
+    	System.out.print(counter);//O/P is 15
+    	}
+    /*	System.out.println("Outside the loop counter is" + counter);
+       The output will be 16
+    */
+    }
+}
+
+/*************While loop****************
+   xxx is also called entry controlled loop
+Before loop = intialization 
+while line = condition
+Aftr Syso(last line) = inc/dec 
+
+i=1;     //intialization 
+while(i<=){   //condition
+ Syso(i);    
+ i++    // inc/dec 
+ 
+1.both for and while 1st chk the cndsn then execute
+2.If we dont give in correct place the prgrm will be go to infinite loop
+*/
+
+
+
+/*************DO While loop****************
+1.xxx is also called exit controlled loop
+2.1st execute then check the condition
+
+do{
+ Syso(i);
+ i++
+ while(i<=);
+ 
+ 
+ */
+
