dcec6b59fa2907b11b7c2e2265679bc3eac02e0b
NANDHA KUMAR S <162905089+NANDHA311223@users.noreply.github.com>
2024-05-10 21:29:05 +0530

    Add files via upload

---
+++ Error Makes Clever/Array1.java
@@ -0,0 +1,13 @@
+package array;
+
+public class Array1 {
+        public static void main(String[] args) {
+			String[] song = new String[4];
+				song[1]="verithanam";
+				song[2]="Mass";
+				song[3]="Aval";
+				System.out.println(song[1]);
+				System.out.println(song[2]);
+				System.out.println(song[3]);
+			}
+	}
