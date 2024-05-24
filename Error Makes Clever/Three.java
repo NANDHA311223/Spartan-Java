d0a2c0e8b0053b6ae58b7e987f0551d7bade3fb4
NANDHA KUMAR S <162905089+NANDHA311223@users.noreply.github.com>
2024-05-24 21:06:27 +0530

    Add files via upload

---
+++ Error Makes Clever/Three.java
@@ -0,0 +1,24 @@
+/*#10 CODING CHALLANGE 2 (PART-1)
+ * Get input for a number and check whether it is
+divisible by both 3 and 5 or not. if yes then
+print, the number is divisible by 3 and 5 print
+the number is not divisible by 3 and 5.*/
+
+package challange2_part1;
+import java.util.Scanner;
+public class Three {
+
+	    public static void main(String[] args) {
+	   
+			boolean hungry=true;
+			boolean icecream=true;
+	
+     if(hungry&&icecream) {
+    	 System.out.println("Eat");
+     }
+     else {
+    	 System.out.println("DOn't Eat");
+     }
+			
+}
+}
