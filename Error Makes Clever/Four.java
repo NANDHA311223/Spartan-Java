c11d22f915469caa8d50e844095631fb23e706a1
NANDHA KUMAR S <162905089+NANDHA311223@users.noreply.github.com>
2024-05-25 23:04:22 +0530

    Add files via upload

---
+++ Error Makes Clever/Four.java
@@ -0,0 +1,17 @@
+//#10 CODING CHALLANGE 2 (PART-1)
+package challange2_part1;
+public class Four {
+      public static void main(String[] args) {
+		   
+				boolean cricket=true;
+				boolean football=false;
+		
+	     if(cricket||football) {
+	    	 System.out.println("play");
+	     }
+	     else {
+	    	 System.out.println("DOn't play");
+	     }
+				
+	}
+	}
