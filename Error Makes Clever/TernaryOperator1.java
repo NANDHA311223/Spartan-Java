0f25813761b614e34837a963fb5bbeba07402c4f
NANDHA KUMAR S <162905089+NANDHA311223@users.noreply.github.com>
2024-05-28 22:44:54 +0530

    Add files via upload

---
+++ Error Makes Clever/TernaryOperator1.java
@@ -0,0 +1,9 @@
+package ternaryOperator;
+//Ternary Operator
+public class TernaryOperator1 {
+      public static void main(String[] args) {
+	    	boolean rain = true;
+			String result = rain?"Take an umbrella":"Enjoy the sun";
+			System.out.println(result);
+		}
+	}
---
+++ Error Makes Clever/TernaryOperator2.java
@@ -0,0 +1,13 @@
+package ternaryOperator;
+import java.util.Scanner;
+public class TernaryOperator2 {
+       public static void main(String[] args) {
+		  System.out.println("Enter two Numbers");
+		 Scanner scan =new Scanner(System.in);
+		 int a = scan.nextInt();
+		 int b = scan.nextInt();
+		 
+	    System.out.println(a>b?"True":"False");
+		 
+	}
+}
