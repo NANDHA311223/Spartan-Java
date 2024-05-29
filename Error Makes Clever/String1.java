bf24d644ac9f67ada31f9de67595dfe9a2f0d2bc
NANDHA KUMAR S <162905089+NANDHA311223@users.noreply.github.com>
2024-05-29 20:42:26 +0530

    Add files via upload

---
+++ Error Makes Clever/String1.java
@@ -0,0 +1,18 @@
+//#09 Compare two Strings
+package string;
+import java.util.Scanner;
+
+public class String1 {
+ 
+	  public static void main(String[] args) {
+		  String f1 = "apple";
+		  String f2 = new String("apple");
+		  
+		  System.out.println(f1.equals(f2));//Compare content/String ("apple")
+		  System.out.println(f1==f2);//Compare reference ( new String )
+		    	   
+		   
+		}
+	}
+
+
