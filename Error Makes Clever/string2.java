a91f2c6df2df3591c70c91d810859fc169ba6012
NANDHA KUMAR S <162905089+NANDHA311223@users.noreply.github.com>
2024-05-30 21:59:00 +0530

    Add files via upload

---
+++ Error Makes Clever/string2.java
@@ -0,0 +1,17 @@
+//#09 Compare two Strings (Using ifElse
+
+package string;
+import java.util.*;
+public class string2 {
+ public static void main(String[] args) {
+	    	 Scanner scan = new Scanner(System.in);
+	    	 String rcb = scan.nextLine();
+	    	 
+			if(rcb.equals("win")) {
+				System.out.println("Ee sala cup namdhae");
+			}
+			else {
+				System.out.println("No Cup");
+			}
+		}
+	}
