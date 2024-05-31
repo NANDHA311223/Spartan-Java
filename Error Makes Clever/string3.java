df30737e732881fdb22e5486d43e65c536209220
NANDHA KUMAR S <162905089+NANDHA311223@users.noreply.github.com>
2024-05-31 20:45:31 +0530

    Add files via upload

---
+++ Error Makes Clever/string3.java
@@ -0,0 +1,22 @@
+package string;
+import java.util.Scanner;
+public class string3 {
+	 
+	
+	public class ifElse5 {
+		public static void main(String[] args) {
+			Scanner Ns = new Scanner(System.in);
+		    String megna=Ns.nextLine();
+		    
+		   if(megna.equals("dead")) {
+			   System.out.println("Surya meets ramya");
+		   }
+		   else {
+			   System.out.println("Surya weds megna");
+		   }
+		}
+
+		}
+
+
+}
