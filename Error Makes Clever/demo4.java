dc844a42aeee4994289cfd8fa161258e976ebe32
NANDHA KUMAR S <162905089+NANDHA311223@users.noreply.github.com>
2024-06-08 15:06:28 +0530

    Add files via upload

---
+++ Error Makes Clever/demo4.java
@@ -0,0 +1,12 @@
+//   For Each Loop   is also called
+//Enhanced fore loop
+public class demo4 {
+    public static void main(String[] args) {
+    
+    	// when we work with array we use for each loop
+  	int num[]= {2,3,4,5};
+     for(int var: num){
+    	System.out.println(var);
+    }
+	}
+}
